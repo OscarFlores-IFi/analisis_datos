0160644b8ae0c4921e787ee99884a5aba855a3c0
Oscar Flores <if715029@gmail.com>
2018-09-03 13:29:47 -0500

    lista de numpy_arrays
    se tienen 6 arreglos de 245*5 datos cada uno.
    Falta concatenarlos para tener un solo arreglo de 245*30

--- with_pandas.py
+++ with_pandas.py
@@ -1,27 +1,21 @@
 ﻿#!/usr/bin/env python
 
+#se importan datos de un archivo .csv y se divide en n columnas cada una de las columnas originales. 
 
 import numpy as np
 import pandas as pd
 
 def datos(archivo, columnas):
-    file = pd.read_csv(archivo)
-    n = len(file['0'])
-    Open = file['Open']
-    m_open = np.zeros((n-columnas,columnas))
-    for k in np.arange(columnas):
-        m_open[:,k] = Open[k:n-columnas+k] 
-    
-    
-    return(file)
-#    retur1n(m_open)
+    file2 = [] #se crea un 'archivo' en blaco, este tendrá las columnas originales redimensionadas. 
+    file = pd.read_csv(archivo) #se importa el archivo.
+    n = len(file['0']) #se calcula la longitud de los datos. 
+    Col_n = list(file.columns.values) #se enlistan las columnas disponibles en el Dataframe
+    for i in Col_n[1:]:
+        columna = file[i] #se accede cada una de las columnas
+        en_blanco = np.zeros((n-columnas,columnas)) #se crea un array en blanco de dimensiones(n-columna * colummna)
+        for k in np.arange(columnas): 
+            en_blanco[:,k] = columna[k:n-columnas+k] #se rellenan los datos vacíos con los originales.
+        file2.append(en_blanco) #se escriben las funciones
+    return(file2)
 file = datos('AC.csv',5)
 print(file)
-
-    
-    
-    
- 
-
-
-
