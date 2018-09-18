35142cc11ba1faae7668340c3a0f87fb9b9aeef9
OscarFlores-IFi <41029452+OscarFlores-IFi@users.noreply.github.com>
2018-09-18 14:08:46 -0500

    k-means de n centroides
    Ahora es posible graficar el total de las inercias de cada centroide ( probado en 10 k-means).

--- k-means.py
+++ k-means.py
@@ -1,21 +1,16 @@
-﻿
+#El algoritmo importa un archivo.csv, encuentra de 1 a 10 centroides y almacena los datos en listas.
+# Sigue con error de no converger en algunas situaciones. como resultado arroja un NaN en total_inercia.
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def datos(archivo):
     ### extraer los datos ###
-    doc = open(archivo, 'r')
-    lines = doc.readlines()
-    doc.close()
-    lines.pop(0) #elimina el titulo de cada uno de las listas
-    semanas = [l.split(",") for l in lines] #recibe los valores en una lista. 
-    for a in range(len(lines)):
-        semanas[a][6]=semanas[a][6][:-1] #elimina el caractér de salto de linea.
-        semanas[a] = [int(l) for l in semanas[a]] #hace número cada uno de los valores de la lista
-    sem = [a[b] for a in [semanas[a:a+7]  for a in range(len(semanas)-7)] for b in range(7)] #toma de 7 en 7 conjuntos de datos y los hace una sola matriz
-    sem = np.array(sem).T #se hace un arreglo (en forma de matriz) de sem en numpy
-    return (sem)
-#    return(np.array(semanas).T)
+    datos = pd.read_csv(archivo,index_col=0)
+    datos = np.array(datos)
+    return (datos)
+
     
 def calc_inercias(semanas,centroides,k):
     inercias = [np.sqrt(((semanas-centroides[l])**2).sum(axis=1)) for l in range(len(k))]
@@ -33,7 +28,7 @@ def calc_centroides(grupos,k):
 def k_means(clusters):
     k = np.ones(clusters)    
         
-    semanas = datos("AC.csv").T
+    semanas = datos("normalizadoAC.csv")
     rand = np.random.randint(len(semanas), size=len(k))
     centroides = [semanas[l] for l in rand]
     
@@ -51,7 +46,22 @@ def k_means(clusters):
         print(total_inercia)
     return(semanas,centroides,grupos,inercias,total_inercia)
 
-semanas,centroides,grupos,inercias,total_inercia = k_means(4)
+### Se ejecuta el algoritmo de k-means en los datos. Posteriormente se guardan los resultados en listas. 
+x = 10
+semanas,centroides,grupos,inercias,total_inercia = [],[],[],[],[]
+for i in range(1,11):
+    print('\n Total_inercia con %i centroides'%i)
+    k_values = k_means(i)
+    semanas.append(k_values[0])
+    centroides.append(k_values[1])
+    grupos.append(k_values[2])
+    inercias.append(k_values[3])
+    total_inercia.append(k_values[4])
+    
 
-
-
+### se grafican las inercias globales (total_inercia) para conocer el 'codo'
+    
+plt.plot(total_inercia)
+plt.xlabel('iteraciones')
+plt.ylabel('inercia global')
+plt.show()
